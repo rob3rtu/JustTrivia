9c7a412ae66529632e3bef4987c33095e773e91a
Robert <robert.udrea@cst.ro>
2024-05-21 20:06:43 +0300

    wip

--- mobile/lib/screens/profile.dart
+++ mobile/lib/screens/profile.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/screens/generic/scaffold_purple.dart';
 
@@ -6,15 +7,20 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ScaffoldPurple(
+    return ScaffoldPurple(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               "Profile screen",
               style: TextStyle(color: Colors.white),
             ),
+            ElevatedButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                child: const Text("log out"))
           ],
         ),
       ),
