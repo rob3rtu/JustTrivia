6e2b5d6c2da309625f7c0354b2a313cb8b25e41d
Robert <robert.udrea@cst.ro>
2024-05-22 21:44:56 +0300

    settings screen

--- mobile/lib/constants/colors.dart
+++ mobile/lib/constants/colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 class AppColors {
   AppColors._();
   static const Color darkPurple = Color.fromRGBO(25, 11, 40, 1);
+  static const Color lightRed = Color.fromRGBO(255, 67, 44, 74);
   static final BoxDecoration transparentWhite = BoxDecoration(
     borderRadius: BorderRadius.circular(16),
     color: const Color.fromRGBO(255, 255, 255, 0.2),
---
+++ mobile/lib/constants/settings_items.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Item {
+  final String name;
+  final IconData icon;
+
+  const Item({
+    required this.name,
+    required this.icon,
+  });
+}
+
+class Category {
+  final String category;
+  final List<Item> items;
+
+  const Category({
+    required this.category,
+    required this.items,
+  });
+}
+
+class SettingsItems {
+  SettingsItems._();
+
+  static const List<Category> list = [
+    Category(
+      category: "Suport",
+      items: [
+        Item(name: "Raporteaza o problema", icon: Icons.bug_report),
+        Item(name: "Sugereaza o noua intrebare", icon: Icons.add),
+      ],
+    ),
+    Category(
+      category: "Cont",
+      items: [
+        Item(name: "Raporteaza o problema", icon: Icons.delete_forever),
+      ],
+    ),
+  ];
+}
